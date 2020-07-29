5ee4e78a68c88034aba8bde6f0f7d5ba517ad09a
osethan <GE47R9xmtouch .zshrcFASEPs@0>
2020-07-29 15:08:21 -0700

    Power set added

---
+++ ctci/chapter8/power_set/power_set.py
@@ -0,0 +1,18 @@
+
+def power_set(aset):
+  """
+  Return all subsets of a set.
+  """
+  def power_set_step(cur, seen, aset):
+    for el in aset:
+      if el not in cur:
+        cur.add(el)
+        if cur not in seen:
+          seen.add(frozenset(cur))
+          power_set_step(cur, seen, aset)
+        cur.remove(el)
+
+  cur = set()
+  seen = {frozenset(cur)}
+  power_set_step(cur, seen, aset)
+  return seen
